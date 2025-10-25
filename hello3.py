26a4368fb3f260d32ddfc25ef898a5029c3f5dab
abhay2sing <abhaysingh480shiv@gmail.com>
2025-10-25 15:17:19 +0530

    first commitssss

--- hello3.py
+++ hello3.py
@@ -1,15 +1,5 @@
 def count_multiples(numbers, divisors):
-    """
-    Counts the occurrences of numbers in a list that are multiples of given divisors.
-
-    Args:
-        numbers (list): The list of numbers to check.
-        divisors (list): The list of numbers to check for as divisors.
-
-    Returns:
-        dict: A dictionary where keys are the divisors and values are the counts
-              of numbers in 'numbers' that are multiples of that divisor.
-    """
+    
     result = {}
     for divisor in divisors:
         count = 0
